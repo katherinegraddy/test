e23e9bce60d0fa776870792c75f94bf58a11aa51
katherine graddy <katherinegraddy10@gmail.com>
2026-04-09 09:41:20 +0100

    Exploring some basic r commands

--- coding_practice.R
+++ coding_practice.R
@@ -1,3 +1,5 @@
+# 0) Working on Absolute Basics ----------------------------------------------
+
 vector <- c(1,2,3)
 list <- c(1, 'two', TRUE)
 
@@ -7,3 +9,26 @@ cat(list)
 combined <- paste (vector, list)
 
 cat(combined)
+
+camp <- c('orojo','gambella','tigray')
+survey_round <- c('R8', 'R10', 'R10')
+hh_count <- c(110, 1500, 200)
+camp_open <- c(T, F, T)
+
+df <- data.frame(survey_round, camp, hh_count, camp_open)
+df
+
+
+survey_round <- sub('R', '', survey_round)
+
+survey_round <- as.numeric(survey_round)
+
+class(survey_round)
+
+
+
+# 1) Data Cleaning Steps --------------------------------------------------
+
+
+
+
